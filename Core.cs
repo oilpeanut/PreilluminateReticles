0cc4f08f3c9685ddd9224d0393ff4bc663edbfc3
oilpeanut <s0ssh1n6@gmail.com>
2025-12-17 00:27:12 +0800

    marked file as v1.0.0

--- Core.cs
+++ Core.cs
@@ -5,7 +5,7 @@ using GHPC.World;
 using GHPC.Equipment.Optics;
 using GHPC.State;
 
-[assembly: MelonInfo(typeof(PreilluminateReticles.Core), "PreilluminateReticles", "0.1.0", "oilpeanut", null)]
+[assembly: MelonInfo(typeof(PreilluminateReticles.Core), "PreilluminateReticles", "1.0.0", "oilpeanut", "https://github.com/oilpeanut/PreilluminateReticles/releases/latest")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace PreilluminateReticles {
