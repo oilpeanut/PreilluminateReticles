4d70e3f7562b92c1fed8d8a523715bfb181d4bda
oilpeanut <s0ssh1n6@gmail.com>
2026-07-22 00:14:23 +0800

    code clean up

--- Core.cs
+++ Core.cs
@@ -6,7 +6,13 @@ using GHPC.Equipment.Optics;
 using GHPC.State;
 using GHPC.Player;
 
-[assembly: MelonInfo(typeof(PreilluminateReticles.Core), "PreilluminateReticles", "1.0.1", "oilpeanut", "https://github.com/oilpeanut/PreilluminateReticles/releases/latest")]
+[assembly: MelonInfo(
+  typeof(PreilluminateReticles.Core),
+  "PreilluminateReticles",
+  "1.0.1",
+  "oilpeanut",
+  "https://github.com/oilpeanut/PreilluminateReticles/releases/latest"
+)]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace PreilluminateReticles {
@@ -17,54 +23,58 @@ namespace PreilluminateReticles {
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
       //makeing sure game is in a scene that has optics to illuminate
-      if(sceneName.StartsWith("LOADER_") || (Char.IsLower(sceneName, 1) && !sceneName.StartsWith("Flex_")/*campaign missions tend to be prefixed with this*/))
-        return;
-      StateController.RunOrDefer(GameState.PlayerReady, new GameStateEventHandler(FindAndIlluminate), GameStatePriority.Lowest);
+      if(
+        sceneName.StartsWith("LOADER_") ||
+        (Char.IsLower(sceneName, 1) && !sceneName.StartsWith("Flex_"))
+        /*campaign missions tend to be prefixed with Flex_*/
+      ) return;
+
+      StateController.RunOrDefer(
+        GameState.PlayerReady,
+        new GameStateEventHandler(FindAndIlluminate),
+        GameStatePriority.Lowest
+      );
     }
 
     public IEnumerator<bool> FindAndIlluminate(GameState gs) {
-      //initializing stuff
       List<Unit> vehiclesInTeam;
       UsableOptic[] parentOptics;
       ReticleMesh[] childReticleMeshes;
-      ReticleEditor.LightState reticleLight;
       Faction playerFaction;
       uint illumCount = 0;
+
       //get list of vehicles in player's faction
       playerFaction = PlayerInput.Instance.CurrentPlayerUnit.Allegiance;
       vehiclesInTeam = SceneUnitsManager.AllUnitsByFaction[(int)playerFaction];
+
       //iterates over the vehicles to find usable optics
       foreach (Unit vic in vehiclesInTeam) {
         parentOptics = vic.gameObject.GetComponentsInChildren<UsableOptic>(true);
         if(parentOptics != null) { 
           foreach(UsableOptic optic in parentOptics) {
+
             //make sure the found optic is day sight
-            if(optic.name == "FLIR" || optic.name == "NVS" || optic.name.Contains("night", StringComparison.CurrentCultureIgnoreCase))
-              continue;
+            if(
+              optic.name == "FLIR" ||
+              optic.name == "NVS" ||
+              optic.name.Contains("night", StringComparison.CurrentCultureIgnoreCase)
+            ) continue;
             childReticleMeshes = optic.gameObject.GetComponentsInChildren<ReticleMesh>(true);
-            //going through reticle meshes in the optics
+
+            //illuminate reticle meshes in the optics
             foreach(ReticleMesh reticleMesh in childReticleMeshes) {
-              reticleLight = reticleMesh.lights[0];
-              //checks that the reticle can and hasn't been illuminated
-              if(!reticleMesh.disableIllumination && reticleLight.light.type == ReticleTree.Light.Type.NightIllumination && reticleLight.value == 0) {
-                if(EnableIllumination(reticleMesh))
-                  illumCount++;
+              if(!reticleMesh.disableIllumination) {
+                reticleMesh.SetLight(ReticleTree.Light.Type.NightIllumination, 1f);
+                illumCount++;
               }
             }
+
           }
         }
       }
+
       LoggerInstance.Msg($"Illuminated {illumCount} reticles");
       yield return true;
-    }
-
-    public bool EnableIllumination(ReticleMesh rm) {
-      if(rm != null && !rm.disableIllumination) {
-        ReticleTree.Light.Type type = ReticleTree.Light.Type.NightIllumination;
-        rm.SetLight(type, 1f);
-        return true;
-      }
-      return false;
     }
   }
 }
